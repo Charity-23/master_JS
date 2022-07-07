6431ec0414c2bd4c3d0c1e7c4048ca700f47d6a8
Charity-23 <charitymamah2@gmail.com>
2022-07-07 07:33:44 -0700

    first commit

---
+++ JS/day_1.js
@@ -0,0 +1,21 @@
+/*Mastering JavaScript day_1
+JavaScript is the programming lang of HTML and the web
+It was invented by Brendan Eich in 1995
+JavaScript output: displays JavaScripts instructions on the screen(window.alert(), document.write(), innerHTML, console.log())
+Statements are set of JavaScript instructions to be executed by the computer.
+Comments marks theJavaScript statements that are not to be executed
+Variables holds/stores the data values
+*/
+
+        var x = 5;
+        var y = 6;
+        var z = x + y;
+        var l = z + y * x;
+        document.getElementById("p01").innerHTML = z;
+        document.getElementById("p02").innerHTML = z;
+        document.getElementById("p05").innerHTML  = l; 
+        console.log(z);
+        document.write (z);
+    
+
+
